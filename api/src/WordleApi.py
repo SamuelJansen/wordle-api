ac5a8b79f51bd3840fdd88ed0e979bb4447a4a02
samuel.jansen <samuel.jansenn@gmail.com>
2022-04-23 02:49:49 -0300

    feature: adding QueueManager plugin

--- api/src/WordleApi.py
+++ api/src/WordleApi.py
@@ -1,6 +1,9 @@
 from flask import render_template
 from python_framework import ResourceManager
+from queue_manager_api import QueueManager
 import ModelAssociation
 
 
-app = ResourceManager.initialize(__name__, ModelAssociation.MODEL)
+app = ResourceManager.initialize(__name__, ModelAssociation.MODEL, managerList=[
+    QueueManager()
+])
